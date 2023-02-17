d1548808793073eb17766de17ebe8c75bc2a9ae2
jhon1996leyder <08jhonleider@gmail.com>
2023-02-17 09:55:33 -0500

    verificar campos del proveedor

--- ApiRes/src/controllers/proveedor.controller.js
+++ ApiRes/src/controllers/proveedor.controller.js
@@ -5,12 +5,12 @@ const add = async (req, res) => {
     
     try {
         const { nombre_proveedor, ubicacion_p, cedula, cuenta_bancaria} = req.body;
-        
+
         if(!nombre_proveedor || !ubicacion_p || !cedula || !cuenta_bancaria ) {
             res.status(400).json({ message: "Bad Request. Please fill all field." });
         }else{
+
             const dato = {cedula, nombre_proveedor, ubicacion_p, cuenta_bancaria};
-            console.log(dato)
             const connection = await getConnection();
             await connection.query(`INSERT INTO proveedor SET ?`, dato);
             res.json( dato );
